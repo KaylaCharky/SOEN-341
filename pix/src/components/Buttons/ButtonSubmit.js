1f90e1fee1c8ae12fb9bc4eb879aeede80298193
olivier-su <olivier_su_@hotmail.com>
2020-02-13 18:48:08 -0500

    Issue #22 Added components for the login page

---
+++ pix/src/components/Buttons/ButtonSubmit.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ButtonSubmit.css';
+ 
+const ButtonSubmit = () => {
+    return (
+       <div>
+          <button type="submit" class="button">Sign In</button>
+       </div>
+    );
+}
+ 
+export default ButtonSubmit;
