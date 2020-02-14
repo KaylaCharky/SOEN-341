8ea4c4a32e77b041636ffec8786fe115e6d03185
kmarsolais <49344244+kmarsolais@users.noreply.github.com>
2020-02-13 23:45:42 -0500

    Preliminary profile page completed

--- pix/src/components/Profile.js
+++ pix/src/components/Profile.js
@@ -2,8 +2,51 @@ import React from "react";
 import "./Navigation.css";
 
 import Button from "@material-ui/core/Button";
+import { makeStyles } from '@material-ui/core/styles';
+import Avatar from '@material-ui/core/Avatar';
 
-const profile = () => {
+// I imported this class from @material-ui in order to modify the size and position of the profile picture
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
+  small: {
+    width: theme.spacing(3),
+    height: theme.spacing(6),
+  },
+  large: {
+    width: theme.spacing(19),
+    height: theme.spacing(19),
+    marginLeft: theme.spacing(2),
+    marginTop: theme.spacing(6),
+    
+  },
+
+  proName:
+  {
+    fontSize: "28px",
+    textAlign: "left",
+    textIndent: "50px",
+        marginTop: theme.spacing(7),
+  },
+
+  numOfPosts: 
+  {
+    fontSize:"15px",
+    marginTop: theme.spacing(4),
+  },
+
+
+
+
+}));
+
+
+export default function ImageAvatars() {
+  const classes = useStyles();
   return (
     //The Whole Background for the page.
     <div class="profile">
@@ -11,16 +54,44 @@ const profile = () => {
       <div id="profile" class="root">
         {/*This is where the profile picture, name, Bio will be found.*/}
         <div id="profile" class="proPic">
-          <div id="proPic" class="logout">
-            <Button variant="contained" color="primary" position="center">
-              Logout
-            </Button>
+
+          <div className= {classes.root} >
+
+          {/* This is where the profile Picture is located*/}
+          <Avatar alt="Profile Picture" src="https://pixtvusa.com/wp-content/uploads/2018/01/pixgalpixtvusa.png" className={classes.large} />
+                  
+                    {/*User information*/}
+                    
+            <div id= "profile" class="proName" className={classes.proName}> 
+            Profile Name <br/>
+                <div id="profile" className={classes.numOfPosts} >
+                  # of posts &emsp;
+                    <div id="profile" style={{display: 'inline-block'}} >
+                    # of followers &emsp;&emsp;
+                    </div>
+                    <div id="profile" style= {{display: 'inline-block'}}>
+                   # following
+                  </div>
+                </div>
+               
+             
+            </div>
+            
+           
+           
+
+          <Button variant="contained" color="primary" className={classes.small}>
+            Logout
+          </Button>
+
+          
+          
           </div>
+         
+         
         </div>
-        {/*User information*/}
-        <div id="profile" class="proInfo">
-          Number of followers, # of posts, # following
-        </div>
+        
+
         {/*Pictures Grid*/}
         <div class="gallery">
           <a
@@ -153,4 +224,5 @@ const profile = () => {
   );
 };
 
-export default profile;
+
+
